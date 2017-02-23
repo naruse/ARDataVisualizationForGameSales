1f82ffd10ee884bb7dc74c02bea937a42e01e80d
Juan Sebastian Munoz <naruse@gmail.com>
2017-02-23 14:36:03 -0600

    Removed license, started to add the data loader

---
+++ Assets/Scripts/DataLoader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+  Created by:
+  Juan Sebastian Munoz Arango
+  naruse@gmail.com
+  all rights reserved
+
+  Data from:
+  https://www.kaggle.com/rush4ratio/video-game-sales-with-ratings/
+ */
+
+public class DataLoader : MonoBehaviour {
+
+    private const string fileToLoadName = "VideoGamesSalesUntil22Dec2016";
+	void Start () {
+
+	}
+
+
+    private void ParseFile() {
+        TextAsset fileContents = Resources.Load(fileToLoadName) as TextAsset;
+        string[] lines = fileContents.text.Split(char.Parse("\n"));
+
+        try {
+
+
+        } catch (System.Exception e) {
+
+            UnityEngine.Debug.LogError("ERROR: " + e.ToString());
+        }
+    }
+}
